1b94c622cdbcb81e0ce50318857f90470a296c25
Юрий Романов Сергеевич <romanov_us@BOLNICA.LOCAL>
2023-07-11 15:40:32 +0300

    Полная версия задач семинара 2

--- C#/HoumeWork1/Program.cs
+++ C#/HoumeWork1/Program.cs
@@ -2,7 +2,7 @@
 // трёхзначное число и на выходе показывает вторую цифру этого числа.
 
 
-/*int CutNumber(int number)
+int CutNumber(int number)
 {
     int dozens = number / 10;
     int unit = dozens %10;
@@ -11,8 +11,8 @@
 }
 int randNumber = new Random().Next(100, 1000);
 int newNumber = CutNumber(randNumber);
-Console.WriteLine($"New version of {randNumber} is {newNumber}");
-*/
+Console.WriteLine($"Введенное число  {randNumber} и вторая цифра {newNumber}");
+
 
 // Task 2. Напишите программу, которая выводит третью цифру заданного числа
 
@@ -26,25 +26,41 @@ int cutThirdDigit(int number)
         return result;
 }
 
-// int randNumber= new Random().Next(0,1000000);
-Console.WriteLine("Введите число не меньше трех знаков: ");
-int randNumber = Convert.ToInt32(Console.ReadLine());
+int randNumber= new Random().Next(0,1000000);
+// Console.WriteLine("Введите число не меньше трех знаков: ");
+// int randNumber = Convert.ToInt32(Console.ReadLine());
  while (randNumber <100)   
     {
     Console.WriteLine("Введено некорректное число");
     Console.WriteLine("Введите корректное число: ");
     randNumber = Convert.ToInt32(Console.ReadLine());
     }
-
-
     int newNumber = cutThirdDigit(randNumber);
-
         
 Console.WriteLine($"Введенное число {randNumber} и третья цифра {newNumber}");
-
-
 
 // Task 3. Напишите программу, которая принимает на вход цифру, 
 // обозначающую день недели, и проверяет, является ли этот день выходным.
 
+int daysWeek(int i)
+{
+    {
+        if (i<=5) Console.WriteLine("Рабочий день - ");
+        else Console.WriteLine("Выходной день - ");
+    }
+    if (i==1) Console.WriteLine("Понедельник");
+    if (i==2) Console.WriteLine("Вторник");
+    if (i==3) Console.WriteLine("Среда");
+    if (i==4) Console.WriteLine("Четверг");
+    if (i==5) Console.WriteLine("Пятница");
+    if (i==6) Console.WriteLine("Суббота");
+    if (i==7) Console.WriteLine("Воскресенье");
+    return i;
+}
 
+int randNumber = new Random().Next(1, 8);
+// Console.WriteLine("Введите число: ");
+// int randNumber = Convert.ToInt32(Console.ReadLine());
+int newNumber = daysWeek(randNumber);
+
+ Console.WriteLine("Введенное число " + newNumber);
